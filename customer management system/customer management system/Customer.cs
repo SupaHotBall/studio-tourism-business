66c5a16ef6171bedfa8be43c569651511c68c757
morrnc1 <morrnc1@student.op.ac.nz>
2023-08-29 00:47:41 +1200

    Customer.cs code added

--- customer management system/customer management system/Customer.cs
+++ customer management system/customer management system/Customer.cs
@@ -1,4 +1,12 @@
 ﻿namespace customer_management_system
 {
+    public class Customer : Person
+    {
+        public int CustomerId { get; set; }
 
+        public Customer(int customerId, string name, string phoneNumber) : base(name, phoneNumber)
+        {
+            CustomerId = customerId;
+        }
+    }
 }
