6e747e136507f3cd0472e2780c9a1bc197f2d868
morrnc1 <morrnc1@student.op.ac.nz>
2023-08-29 00:44:28 +1200

    Person.cs code added

--- customer management system/customer management system/Person.cs
+++ customer management system/customer management system/Person.cs
@@ -1,4 +1,14 @@
 ﻿namespace customer_management_system
 {
-    
+    public class Person
+    {
+        public string Name { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public Person(string name, string phoneNumber)
+        {
+            Name = name;
+            PhoneNumber = phoneNumber;
+        }
+    }
 }
