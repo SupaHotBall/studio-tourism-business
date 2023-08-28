00624048af29dc8c68d74d115369fa98c9012e96
morrnc1 <morrnc1@student.op.ac.nz>
2023-08-29 00:52:21 +1200

    Booking.cs code added

--- customer management system/customer management system/Booking.cs
+++ customer management system/customer management system/Booking.cs
@@ -1,4 +1,25 @@
-﻿namespace customer_management_system
-{
+﻿using System.Collections.Generic;
+using System;
 
+namespace customer_management_system
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime BookingDate { get; set; }
+        public List<Service> Services { get; set; } = new List<Service>();
+
+        public Booking(int bookingId, Customer customer, DateTime bookingDate)
+        {
+            BookingId = bookingId;
+            Customer = customer;
+            BookingDate = bookingDate;
+        }
+
+        public void AddService(Service service)
+        {
+            Services.Add(service);
+        }
+    }
 }
