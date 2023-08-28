e2281d9d3995e0f9f18a8ca32ee34af92c4ac5e6
morrnc1 <morrnc1@student.op.ac.nz>
2023-08-29 00:49:38 +1200

    Service.cs code added

--- customer management system/customer management system/Service.cs
+++ customer management system/customer management system/Service.cs
@@ -1,4 +1,14 @@
 ﻿namespace customer_management_system
 {
+    public class Service
+    {
+        public string ServiceName { get; set; }
+        public decimal Price { get; set; }
 
+        public Service(string serviceName, decimal price)
+        {
+            ServiceName = serviceName;
+            Price = price;
+        }
+    }
 }
