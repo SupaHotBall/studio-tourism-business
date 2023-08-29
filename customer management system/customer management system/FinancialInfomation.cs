445fee29d54d21b4be07abb336286c3120c5a00e
SupaHotBall <lees27@student.op.ac.nz>
2023-08-29 21:32:44 +1200

    created financial class

--- customer management system/customer management system/FinancialInfomation.cs
+++ customer management system/customer management system/FinancialInfomation.cs
@@ -4,6 +4,7 @@ namespace customer_management_system
 {
     public class FinancialInfomation
     {
-        
+        List<string> FinancialName = new List<string>();
+        List<int> FinancialCosts = new List<int>();
     }
 }
