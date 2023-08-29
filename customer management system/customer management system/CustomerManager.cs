960c8ad6b8299171c0c73668d41dc5926195526d
morrnc1 <morrnc1@student.op.ac.nz>
2023-08-29 14:13:16 +1200

    Compleating CustomerManager.cs (needs review)

--- customer management system/customer management system/CustomerManager.cs
+++ customer management system/customer management system/CustomerManager.cs
@@ -1,6 +1,22 @@
-﻿namespace customer_management_system
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace customer_management_system
 {
     internal class CustomerManager
     {
+        private static readonly string FilePath = "customers.txt";
+        public static List<Customer> LoadCustomersFromFile()
+        {
+            if (!File.Exists(FilePath)) return new List<Customer>();
+
+            var lines = File.ReadAllLines(FilePath);
+            return lines.Select(line =>
+            {
+                var parts = line.Split(',');
+                return new Customer(int.Parse(parts[0]), parts[1], parts[2]);
+            }).ToList();
+        }
     }
 }
